7f954ea84fd8811d2097679a1d2a8cac9682fdf8
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-07-31 18:13:18 -0400

    caesar pseudo

---
+++ pset2/caesarsCipher.c
@@ -0,0 +1,10 @@
+ask user for input using get_int()
+check program was run properly with proper input
+  if() not return main
+prompt user for plaintext to ciper using get_string
+  if() not return main
+iterate over string
+check for case
+cycle charecters down alphabet array? rotating desired amount preserving case
+printf ciphered output in terminal
+return
