f82381adfc6445fb94d7cf492bc1741e6330ce63
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-08-02 12:31:34 -0400

    caesar partial

--- pset2/caesarsCipher.c
+++ pset2/caesarsCipher.c
@@ -8,3 +8,37 @@ check for case
 cycle charecters down alphabet array? rotating desired amount preserving case
 printf ciphered output in terminal
 return
+
+
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(int argc, string argv[])
+{
+    if(argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        printf("1\n");
+        return 1;
+    }
+    else if (argc == 2)
+    {
+        int key = 0;
+        
+        if(argv[1])
+        {
+            key = atoi(argv[1]);
+        }
+        else
+        {
+            return 1;
+        }
+        
+        printf("success\n");
+        printf("%d\n", key);
+    }
+}
